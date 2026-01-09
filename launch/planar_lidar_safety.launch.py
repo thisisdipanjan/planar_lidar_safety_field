884b6360f765ddbcd19acba3e79093bd802d5d3a
Dipanjan Maji <mdipanjan2002@gmail.com>
2026-01-09 06:15:35 +0000

    updated files and folders

--- launch/planar_lidar_safety.launch.py
+++ launch/planar_lidar_safety.launch.py
@@ -5,8 +5,8 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='planar_lidar_safety_field',
-            executable='planar_lidar_lidar_safety_node',
-            name='planar_lidar_lidar_safety',
+            executable='planar_lidar_safety_node',
+            name='planar_lidar_safety',
             parameters=['config/planar_lidar_safety_params.yaml']
         )
     ])
